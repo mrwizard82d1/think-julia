a5d01e1bb956a8547c348fc6f297648e1a69ba09
laj <larry.jones@reveal-energy.com>
2021-11-01 09:58:56 -0500

    Programming basics

---
+++ 01-way-of-program.jl
@@ -0,0 +1,26 @@
+1 + 1
+
+# First "program"
+println("Hello, Julia World!")
+
+# Operators
+
+40 + 2
+43 - 1
+6 * 7
+
+84 / 2
+
+6^2 + 6
+
+# Values and types
+
+typeof(2)
+typeof(42.0)
+typeof("Hello, World")
+typeof("2")
+typeof("42.0")
+
+# Not the result you may expect
+1,000,000  # Not a million
+1_000_000  # Julia understands the underscore as a separator
