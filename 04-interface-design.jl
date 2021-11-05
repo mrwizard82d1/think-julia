c1953c27aa9787182876d64984cccf722e09f250
laj <larry.jones@reveal-energy.com>
2021-11-05 11:04:40 -0500

    Create environment and add packages

---
+++ 04-interface-design.jl
@@ -0,0 +1,12 @@
+# Case study: interface design
+
+# Uses the `ThinkJulia` module that is available from
+# `https://github.com/benLauwens/ThinkJulia.jl`. 
+#
+# Create an environment and add this package using the package
+# manager mode. (Entered by pressing the right bracket 
+# (']') key.)
+#
+# Although the book describes using `JuliaBox` to run the code,
+# I have decided to use `IJulia` instead. (Must also be added to
+# the environment.)
