08ed1096ca5be3b066da5c64617fe6aba22d47b1
laj <larry.jones@reveal-energy.com>
2021-11-06 14:54:53 -0500

    Add docstring to function

--- 04-interface-design.jl
+++ 04-interface-design.jl
@@ -10,3 +10,17 @@
 # Although the book describes using `JuliaBox` to run the code,
 # I have decided to use `IJulia` instead. (Must also be added to
 # the environment.)
+
+"""
+polyline(t, n, len, angle)
+
+Draws `n` line segments, each of length, `len`, and rotated by 
+`angle` (degrees) relative to the previous line line segment, 
+using the turtle, `t`
+"""
+function polyline(t, n, len, angle)
+    for i in 1:n
+        forward(t, len)
+        turn(t, angle)
+    end
+end
