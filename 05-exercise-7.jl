2807fc10eeee250db2cbe44390d7550b9ba72971
laj <larry.jones@reveal-energy.com>
2021-11-10 10:52:12 -0600

    First two parts of exercise 5-7
    This commit implements drawing a Koch curve and a Koch snowflake.

---
+++ 05-exercise-7.jl
@@ -0,0 +1,66 @@
+# Exercise 5-7
+
+# Koch curve
+
+using ThinkJulia
+
+"""
+koch(t, x)
+
+Draw a Koch curve.
+
+Args:
+    t: turtle used for drawing
+    x: integer length to be drawn
+"""
+function koch(t, x)
+    if x == 0
+        return
+    end
+
+    if x < 3
+        forward(t, x)
+    end
+
+    koch(t, x ÷ 3)
+    turn(t, -60)
+
+    koch(t, x ÷ 3)
+    turn(t, 120)
+
+    koch(t, x ÷ 3)
+    turn(t, -60)
+
+    koch(t, x ÷ 3)
+end
+
+function draw_koch(x)
+    turtle = Turtle()
+    @svg begin
+        koch(turtle, x)
+    end
+end
+
+"""
+snowflake(t, x)
+
+Draw a snowflake using three Koch curves.
+
+Args:
+    t: turtle used for drawing
+    x: length of snowflake / Koch curve
+"""
+function snowflake(t, x)
+    koch(t, x)
+    turn(t, 120)
+    koch(t, x)
+    turn(t, 120)
+    koch(t, x)
+end
+
+function draw_snowflake(x)
+    turtle = Turtle()
+    @svg begin
+        snowflake(turtle, x)
+    end
+end
