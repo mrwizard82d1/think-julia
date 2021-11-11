bb68f67db25bbfdbb58337f45e5bc1db117b84e5
laj <larry.jones@reveal-energy.com>
2021-11-11 09:47:43 -0600

    Chapter 6 incremental development

--- 06-fruitful-functions.jl
+++ 06-fruitful-functions.jl
@@ -59,3 +59,74 @@ end
 show(broken_absvalue(-1))
 show(broken_absvalue(0))  # Use `show` to display returned `nothing` value
 show(broken_absvalue(1))
+
+# Incremental development
+
+# First step: start with (very) small but working code
+function distance(x₁, y₁, x₂, y₂)
+    0.0
+end
+distance(0, 0, 0, 0)
+
+# This only passes some arguments.
+distance(1, 2, 4, 6)  # values result in 3^2 + 4^2 or 5
+
+# Add code to handle original case
+function distance(x₁, y₁, x₂, y₂)
+    dx = x₂ - x₁
+    dy = y₂ - y₁
+    @show dx dy  # shows arguments and then moves on
+    0.0
+end
+distance(0, 0, 0, 0)
+
+# But does not yet implement failing case
+distance(1, 2, 4, 6)
+
+# Continue changing implementation to handle failing case but only pass original
+function distance(x₁, y₁, x₂, y₂)
+    dx = x₂ - x₁
+    dy = y₂ - y₁
+    d² = dx^2 + dy^2
+    @show d²  # shows arguments and then moves on
+    0.0
+end
+distance(0, 0, 0, 0)
+distance(1, 2, 4, 6)
+
+# Change implementation to pass both tests
+function distance(x₁, y₁, x₂, y₂)
+    dx = x₂ - x₁
+    dy = y₂ - y₁
+    d² = dx^2 + dy^2
+    @show d²  # shows arguments and then moves on
+    sqrt(d²)
+end
+distance(0, 0, 0, 0)
+distance(1, 2, 4, 6)
+
+
+# Remove debugging code
+function distance(x₁, y₁, x₂, y₂)
+    dx = x₂ - x₁
+    dy = y₂ - y₁
+    d² = dx^2 + dy^2
+    sqrt(d²)
+end
+distance(0, 0, 0, 0)
+distance(1, 2, 4, 6)
+
+# Although not needed here, refactor to remove intermediate, temporary values
+function distance(x₁, y₁, x₂, y₂)
+    d² = (x₂ - x₁)^2 + (y₂ - y₁)^2
+    sqrt(d²)
+end
+distance(0, 0, 0, 0)
+distance(1, 2, 4, 6)
+
+# Final refactoring
+function distance(x₁, y₁, x₂, y₂)
+    sqrt((x₂ - x₁)^2 + (y₂ - y₁)^2)
+end
+distance(0, 0, 0, 0)
+distance(1, 2, 4, 6)
