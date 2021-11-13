aa24349b29db0cd9b89a9ae3226e26b45034e2f5
laj <larry.jones@reveal-energy.com>
2021-11-12 22:13:12 -0600

    Finish chapter 7
    This commit finishes the code from chapter 7

--- 07-iteration.jl
+++ 07-iteration.jl
@@ -7,7 +7,6 @@ println(x)
 x = 7
 println(x)
 
-
 a = 5
 b = a
 a == b
@@ -46,3 +45,57 @@ function seq(n)
 end
 seq(16)
 seq(3)
+
+# Break
+
+while true
+    print("> ")
+    line = readline()
+    if line == "done"
+        break
+    end
+    println(line)
+end
+println("Done!")
+
+for i in 1:10
+    if i % 3 == 0
+        continue
+    end
+    print(i, " ")
+end
+
+# Square root
+
+a = 4
+x = 3
+y = (x + a/x) / 2
+x = y
+y = (x + a/x) / 2
+x = y
+y = (x + a/x) / 2
+x = y
+y = (x + a/x) / 2
+x = y
+y = (x + a/x) / 2
+
+x = 4
+while true
+    println(x)
+    y = (x + a/x) / 2
+    if y == x
+        break
+    end
+    x = y
+end
+
+x = 5
+ε = 1e-7
+while true
+    println(x)
+    y = (x + a/x) / 2
+    if abs(y - x) < ε
+        break
+    end
+    x = y
+end
