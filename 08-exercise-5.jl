92e95c54e6a8965c46438a1d4291aabdd4d9bc89
laj <larry.jones@reveal-energy.com>
2021-11-16 09:45:34 -0600

    Change implementation of count to use find
    Previous commits looped through all the letters using a for loop
    
    This commit reuses `find` written in a previous exercise.

--- 08-exercise-5.jl
+++ 08-exercise-5.jl
@@ -2,12 +2,26 @@
 
 # count()
 
+function find(word, letter, start)
+    index = start
+    while index <= sizeof(word)
+        if word[index] == letter
+            return index
+        end
+        index = nextind(word, index)
+    end
+    -1
+end
+
+
 function count(text, to_find)
     counter = 0
-    for candidate in text
-        if candidate == to_find
-            counter = counter + 1
-        end
+    start_at = 1
+    found_at = find(text, to_find, start_at) 
+    while found_at != -1
+        counter = counter + 1
+        start_at = nextind(text, found_at)
+        found_at = find(text, to_find, start_at)
     end
     counter
 end
